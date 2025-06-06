1bc2253951eb36665d3f934cb1f45a4195451038
Zhan1983Ye <154672611+Zhan1983Ye@users.noreply.github.com>
2025-06-06 11:08:10 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,7 +31,7 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "zhanye"         # 用户名
+USER_NAME = "Zhan1983Ye"         # 用户名
 UUID = "962521c0-e7d5-4dee-aacc-ea34698bae04"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
 DOMAIN = "shouyoukeji.pp.ua"                   # 域名，留空则自动获取
